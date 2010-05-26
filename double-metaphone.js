84a53f35a46bef3d0fdbf0b6273d7ef7425056ba
Moritz Peters <moritz@mpeters.biz>
2010-05-26 11:40:40 -0700

    Improve loop... probably won't change much though.

--- double-metaphone.js
+++ double-metaphone.js
@@ -977,7 +977,7 @@ function string_at(string, start, length, list)
     if ((start <0) || (start >= string.length))
       return 0;
 
-    for (i=0; i<list.length; i++) {
+    for (var i=0, len=list.length; i<len; i++) {
       if (list[i] == string.substr(start, length))
         return 1;
     }
